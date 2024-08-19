5ef3d50acba133d9ff3748d85d7afb01c42474c8
douglaswe <djfq13@hotmail.com>
2024-08-19 08:08:29 -0300

    first commit

---
+++ src/test/java/aula/mineda/aula1/Aula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package aula.mineda.aula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
